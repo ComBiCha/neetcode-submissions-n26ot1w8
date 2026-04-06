83288879fc87e578759c3ef0011a3e8aff54773c
Trần Hồ Thanh Sang <151933192+ComBiCha@users.noreply.github.com>
2026-04-06 16:06:02 +0700

    Add: buy-and-sell-crypto - submission-8

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-8.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int MaxProfit(int[] prices) {
+        int minPrice = prices[0];
+        int maxProfit = 0;
+        for(int i = 0; i < prices.Length; i++)
+        {
+            maxProfit = Math.Max(maxProfit, prices[i] - minPrice);
+            minPrice = Math.Min(minPrice, prices[i]);
+        }
+        return maxProfit;
+    }
+}
